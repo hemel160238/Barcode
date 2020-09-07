7711efc11c9b7bf6f71cdef1aa6bce39abdf9c2f
Tanvir Hemel <tanvirhemel9208@gmail.com>
2020-09-07 23:18:05 +0600

    Add Student Search

--- purchase.js
+++ purchase.js
@@ -1,85 +1,3 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-    <title>Make Purchase</title>
-
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
-        integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"
-        defer></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
-        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"
-        defer></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
-        integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"
-        defer></script>
-    <script src="purchase.js" defer></script>
-
-</head>
-
-<body>
-    <h1>Add Item to Basket</h1>
-
-    <div>
-        <div>
-            <input id="productIn" type="text" class="form-control" name="productId" placeholder="Product Id">
-            <button type="button" id="addItemButton" onclick="myFunction()" class="btn btn-success">Add To
-                Basket</button>
-        </div>
-
-        <div>
-            <input type="text" id="sname" name="sname" placeholder="Student Id"><br>
-            <button type="button" id="addStudentButton" class="btn btn-success">Select
-                Student</button>
-
-        </div>
-    </div>
-    <div>
-        <form>
-            <fieldset disabled>
-                <div class="form-group">
-                    <label for="disabledTextInput">Id</label>
-                    <input type="text" id="disabledTextInput" class="form-control" placeholder="ID">
-                </div>
-                <div class="form-group">
-                    <label for="disabledTextInput">Name</label>
-                    <input type="text" id="disabledTextInput" class="form-control" placeholder="Name">
-                </div>
-                <div class="form-group">
-                    <label for="disabledTextInput">Email</label>
-                    <input type="text" id="disabledTextInput" class="form-control" placeholder="Email">
-                </div>
-                <div class="form-group">
-                    <label for="disabledTextInput">Credit</label>
-                    <input type="text" id="disabledTextInput" class="form-control" placeholder="Credit">
-                </div>
-            </fieldset>
-        </form>
-    </div>
-
-
-
-    <table id="myTable" class="table table-dark">
-        <thead>
-            <tr>
-                <th scope="col">Id</th>
-                <th scope="col">Price</th>
-                <th scope="col">Name</th>
-                <th scope="col">Qty</th>
-                <th scope="col">Action</th>
-            </tr>
-        </thead>
-
-        <!-- <tfoot>
-            <tr>
-                <td>My footer</td>
-            </tr>
-        </tfoot> -->
-    </table>
-
-    <script>
 
         // Get the input field
         var input = document.getElementById("productIn");
@@ -149,8 +67,8 @@
             http.send(params);
         }
 
-        function getStudentInfo() {
-            var studentId = document.getElementById("sname").value;
+        function getStudentInfo(btn) {
+            var studentId = document.getElementById("studentId").value;
 
             //console.log(btn);
 
@@ -179,7 +97,3 @@
             var row = btn.parentNode.parentNode;
             row.parentNode.removeChild(row);
         }
-    </script>
-</body>
-
-</html>
--- student.php
+++ student.php
@@ -2,13 +2,13 @@
 session_start();
 include_once("config.php");
 
-$productId = NULL;
+$studentId = NULL;
 
-if (isset($_POST['productId'])) {
-    $productId = $_POST['productId'];
-    //echo $productId;
+if (isset($_POST['studentId'])) {
+    $studentId = $_POST['studentId'];
+    //echo $studentId;
 }
-$get_purchase = get_item($productId);
+$get_purchase = get_student($studentId);
 
 $result_purchase = $get_purchase->fetchAll();
 $json_string = json_encode($result_purchase);
@@ -16,10 +16,10 @@ $json_string = json_encode($result_purchase);
 echo $json_string;
 //SELECT * FROM `product` WHERE id = 1010 
 
-function get_item($productId)
+function get_student($studentId)
 {
     $con = config::connect();
-    $query_string = "SELECT * FROM `product` WHERE id = ".$productId;
+    $query_string = "SELECT * FROM `student` WHERE id = ".$studentId;
     $query = $con->prepare($query_string);
 
     $query->execute();
